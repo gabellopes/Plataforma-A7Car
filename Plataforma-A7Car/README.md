85e0e8af728e120e9e9762ee895773c37a36d2ed
Gabriel Alves <gabriel.alves61@aluno.cps.sp.gov.br>
2026-08-31 20:14:04 -0300

    Delete Plataforma-A7Car directory

--- Plataforma-A7Car/README.md
+++
@@ -1,3 +0,0 @@
-# Plataforma-A7Car
-TCC...
-.
--- Plataforma-A7Car/tcc/php/salvar/Conexao.php
+++
@@ -1,6 +0,0 @@
-<?php 
-$sql = new mysqli("localhost", "root", "", "concessionaria");
-if ($sql->connect_errno) {
-    echo "Falha na conexão: (" . $sql->connect_errno . ") " . $sql->connect_error;
-}
-?>
