03b92b67963dbee846264171a02467647b9d9796
giovanna <giovannaaraujocunha6@gmail.com>
2026-09-08 10:12:51 -0300

    Update Cadastro.php

--- php/user/Cadastro.php
+++ php/user/Cadastro.php
@@ -89,7 +89,7 @@ session_start();
                 </h3>
             </div>
             <div id="divadm">
-                <a href="../admin/Clientes.php" id="adm">ÁREA ADMINISTRATIVA</a>
+                <a href="../admin/admin.php" id="adm">ÁREA ADMINISTRATIVA</a>
             </div>
         </div>
     </footer>
